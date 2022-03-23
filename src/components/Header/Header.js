ef7e375cf04dfa66ad26bfb3d0515646fea6b08a
avsssai <avsssai@gmail.com>
2022-03-23 14:57:45 +0530

    completed version

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,73 +1,96 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import React from "react";
+import styled from "styled-components/macro";
 
-import { COLORS, WEIGHTS } from '../../constants';
-import Logo from '../Logo';
-import SuperHeader from '../SuperHeader';
-import MobileMenu from '../MobileMenu';
-
+import { COLORS, QUERIES, WEIGHTS } from "../../constants";
+import Logo from "../Logo";
+import SuperHeader from "../SuperHeader";
+import MobileMenu from "../MobileMenu";
+import Icon from "../Icon";
 const Header = () => {
-  const [showMobileMenu, setShowMobileMenu] = React.useState(false);
+	const [showMobileMenu, setShowMobileMenu] = React.useState(false);
 
-  // For our mobile hamburger menu, we'll want to use a button
-  // with an onClick handler, something like this:
-  //
-  // <button onClick={() => setShowMobileMenu(true)}>
+	// For our mobile hamburger menu, we'll want to use a button
+	// with an onClick handler, something like this:
+	//
+	// <button onClick={() => setShowMobileMenu(true)}>
 
-  return (
-    <header>
-      <SuperHeader />
-      <MainHeader>
-        <Side>
-          <Logo />
-        </Side>
-        <Nav>
-          <NavLink href="/sale">Sale</NavLink>
-          <NavLink href="/new">New&nbsp;Releases</NavLink>
-          <NavLink href="/men">Men</NavLink>
-          <NavLink href="/women">Women</NavLink>
-          <NavLink href="/kids">Kids</NavLink>
-          <NavLink href="/collections">Collections</NavLink>
-        </Nav>
-        <Side />
-      </MainHeader>
+	return (
+		<header>
+			<SuperHeader />
+			<MainHeader>
+				<Side>
+					<Logo />
+				</Side>
+				<Nav>
+					<NavLink href='/sale'>Sale</NavLink>
+					<NavLink href='/new'>New&nbsp;Releases</NavLink>
+					<NavLink href='/men'>Men</NavLink>
+					<NavLink href='/women'>Women</NavLink>
+					<NavLink href='/kids'>Kids</NavLink>
+					<NavLink href='/collections'>Collections</NavLink>
+				</Nav>
+				<Side />
+				<MobileSide>
+					<NavLink href='/shop'>
+						<Icon id='shopping-bag' />
+					</NavLink>
+					<NavLink href='#'>
+						<Icon id='search' />
+					</NavLink>
+					<NavLink href='#'>
+						<Icon id='menu' onClick={() => setShowMobileMenu(true)} />
+					</NavLink>
+				</MobileSide>
+			</MainHeader>
 
-      <MobileMenu
-        isOpen={showMobileMenu}
-        onDismiss={() => setShowMobileMenu(false)}
-      />
-    </header>
-  );
+			<MobileMenu isOpen={showMobileMenu} onDismiss={() => setShowMobileMenu(false)} />
+		</header>
+	);
 };
 
 const MainHeader = styled.div`
-  display: flex;
-  align-items: baseline;
-  padding: 18px 32px;
-  height: 72px;
-  border-bottom: 1px solid ${COLORS.gray[300]};
+	display: flex;
+	align-items: baseline;
+	padding: 18px 32px;
+	height: 72px;
+	border-bottom: 1px solid var(--color-gray-300); ;
 `;
 
 const Nav = styled.nav`
-  display: flex;
-  gap: 48px;
-  margin: 0px 48px;
+	display: flex;
+	gap: clamp(1rem, 48px, 3.5rem);
+	margin: 0px 48px;
+	flex: 2;
+	justify-content: space-between;
+	@media ${QUERIES.tabletAndDown} {
+		display: none;
+	}
 `;
 
 const Side = styled.div`
-  flex: 1;
+	flex: 1;
+`;
+
+const MobileSide = styled.div`
+	display: flex;
+
+	gap: 16px;
+	justify-content: space-between;
+	@media ${QUERIES.tabletAndUp} {
+		display: none;
+	}
 `;
 
 const NavLink = styled.a`
-  font-size: 1.125rem;
-  text-transform: uppercase;
-  text-decoration: none;
-  color: ${COLORS.gray[900]};
-  font-weight: ${WEIGHTS.medium};
+	font-size: 1.125rem;
+	text-transform: uppercase;
+	text-decoration: none;
+	color: var(--color-gray-900);
+	font-weight: ${WEIGHTS.medium};
 
-  &:first-of-type {
-    color: ${COLORS.secondary};
-  }
+	&:first-of-type {
+		color: var(--color-secondary);
+	}
 `;
 
 export default Header;
--- src/components/MobileMenu/MobileMenu.js
+++ src/components/MobileMenu/MobileMenu.js
@@ -1,37 +1,88 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components/macro';
-import { DialogOverlay, DialogContent } from '@reach/dialog';
+import React from "react";
+import styled from "styled-components/macro";
+import { DialogOverlay, DialogContent } from "@reach/dialog";
 
-import { QUERIES } from '../../constants';
+import { QUERIES } from "../../constants";
 
-import UnstyledButton from '../UnstyledButton';
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import UnstyledButton from "../UnstyledButton";
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
 const MobileMenu = ({ isOpen, onDismiss }) => {
-  if (!isOpen) {
-    return null;
-  }
+	if (!isOpen) {
+		return null;
+	}
 
-  return (
-    <div>
-      <button onClick={onDismiss}>Dismiss menu</button>
-      <nav>
-        <a href="/sale">Sale</a>
-        <a href="/new">New&nbsp;Releases</a>
-        <a href="/men">Men</a>
-        <a href="/women">Women</a>
-        <a href="/kids">Kids</a>
-        <a href="/collections">Collections</a>
-      </nav>
-      <footer>
-        <a href="/terms">Terms and Conditions</a>
-        <a href="/privacy">Privacy Policy</a>
-        <a href="/contact">Contact Us</a>
-      </footer>
-    </div>
-  );
+	return (
+		<Wrapper onDismiss={onDismiss} isOpen={isOpen}>
+			<Content>
+				<CloseButton onClick={onDismiss}>
+					<Icon id='close' />
+				</CloseButton>
+				<NavLinks>
+					<NavLink href='/sale'>Sale</NavLink>
+					<NavLink href='/new'>New&nbsp;Releases</NavLink>
+					<NavLink href='/men'>Men</NavLink>
+					<NavLink href='/women'>Women</NavLink>
+					<NavLink href='/kids'>Kids</NavLink>
+					<NavLink href='/collections'>Collections</NavLink>
+				</NavLinks>
+				<Footer>
+					<NavLinkFooter href='/terms'>Terms and Conditions</NavLinkFooter>
+					<NavLinkFooter href='/privacy'>Privacy Policy</NavLinkFooter>
+					<NavLinkFooter href='/contact'>Contact Us</NavLinkFooter>
+				</Footer>
+			</Content>
+		</Wrapper>
+	);
 };
+
+const Wrapper = styled(DialogOverlay)`
+	position: fixed;
+	top: 0;
+	left: 0;
+	bottom: 0;
+	right: 0;
+	background: hsl(0deg 0% 0% / 0.5);
+`;
+
+const CloseButton = styled(UnstyledButton)`
+	position: absolute;
+	top: 16px;
+	right: 16px;
+`;
+const Content = styled(DialogContent)`
+	position: absolute;
+	right: 0;
+	height: 100%;
+	width: 60%;
+	background: white;
+`;
+
+const NavLinks = styled.nav`
+	display: flex;
+	flex-direction: column;
+	gap: 8px;
+	padding: 1.5rem;
+`;
+
+const NavLink = styled.a`
+	text-decoration: none;
+	font-size: 1rem;
+	color: inherit;
+	font-weight: 600;
+`;
+
+const Footer = styled.footer`
+	display: flex;
+	font-size: 0.725rem;
+	justify-content: space-evenly;
+`;
+
+const NavLinkFooter = styled(NavLink)`
+	font-size: 0.75rem;
+	font-weight: 400;
+`;
 
 export default MobileMenu;
